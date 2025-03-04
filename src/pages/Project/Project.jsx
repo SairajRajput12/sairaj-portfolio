9eb68691876ac8cebbcf006670a2532e98b1bdc4
SairajRajput12 <sairajrajput6@gmail.com>
2025-03-04 18:28:12 +0530

    completed projects

--- src/pages/Project/Project.jsx
+++ src/pages/Project/Project.jsx
@@ -3,7 +3,7 @@ import './Project.css';
 export default function Project(){
     return(
         <section className='project'>
-            <h1>This is the Project component</h1>
+            <h1>Project Work</h1>
         </section>
     )
 }
