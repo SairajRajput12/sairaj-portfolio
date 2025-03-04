73788124f02d618e37537df49edb71be63f6bd2e
SairajRajput12 <sairajrajput6@gmail.com>
2025-03-04 11:53:46 +0530

    skills component completed

--- src/pages/About/AboutMe.jsx
+++ src/pages/About/AboutMe.jsx
@@ -8,7 +8,7 @@ export default function About(){
       <div className='about-me'>
        <h1>About Me</h1>
         <img className='profile-image' src='makima.jpeg' alt='makima image'  />
-        <div className='description'>
+          <div className='description'>
           <p className='desc'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
           <p className='desc'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
           <p className='desc'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
