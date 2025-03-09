0179f7cf7f0b957670d444e3b0f2137f689ff9a1
SairajRajput12 <sairajrajput6@gmail.com>
2025-03-09 09:44:51 +0530

    fixed linkedine url

--- src/pages/About/SocialMediaIcons.jsx
+++ src/pages/About/SocialMediaIcons.jsx
@@ -3,7 +3,7 @@ import "./social.css"; // Importing the CSS file
 const socialMedia = [
     { path: "github.png", name: "Github Profile", link: "https://github.com/SairajRajput12" },
     { path: "instagram.png", name: "Instagram Profile", link: "https://www.instagram.com/sairaj2172/" },
-    { path: "linkedin.png", name: "LinkedIn Profile", link: "https://www.linkedin.com/in/sairajrajputit/" },
+    { path: "linkedin.png", name: "LinkedIn Profile", link: "https://www.linkedin.com/in/sairajrajput-ps/" },
     { path: "twitter.png", name: "Twitter Profile", link: "https://x.com/sairaj026" },
     { path: "Leetcode.png", name: "Leetcode Profile", link: "https://leetcode.com/u/Vegabond_12/" }
 ];
